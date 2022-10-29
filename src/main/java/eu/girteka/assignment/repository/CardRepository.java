7c3150deb256c177fec1ed9d25dc65f11df17dc8
Nikoloz Chapidze <nick.chapidze@gmail.ge>
2022-10-30 01:10:52 +0400

    added repositories

---
+++ src/main/java/eu/girteka/assignment/repository/CardRepository.java
@@ -0,0 +1,9 @@
+package eu.girteka.assignment.repository;
+
+import eu.girteka.assignment.model.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card, Long> {
+}
